6e5f60b2795d3927c2a5aa0f131c07340e7a745f
rampagingsquirrel <nicholas.doucette@gmail.com>
2020-12-05 17:31:58 -0500

    Added scaling & reset functionality to controls

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,11 +3,20 @@ const windowSize = 480; // Gameplay window size (both height & width)
 const gridMin = 8;
 const gridMax = 96;
 
+const btnGrow = document.querySelector('#btnGrow');
+const btnShrink = document.querySelector('#btnShrink');
+const btnReset = document.querySelector('#btnReset');
+
 // Default values for grid size & cells (both height & width)
 // cell size is determined by windowsize / gridsize (480/16 = 30)
+// numberOfCells is gridSize * gridSize
 let gridSize = 16;
-let cellsize = 30;
+let cellSize = 30;
 let numberOfCells = 256;
+
+btnGrow.addEventListener('click', () => { GrowGrid(); });
+btnShrink.addEventListener('click', () => { ShrinkGrid(); });
+btnReset.addEventListener('click', () => { ResetGrid(); });
 
 PopulateGrid();
 
@@ -19,10 +28,67 @@ function PopulateGrid() {
 
         let newCell = document.createElement('div');
         newCell.className = "gridCell";
+        newCell.style.width = `${cellSize}px`;
+        newCell.style.height = `${cellSize}px`;
 
         grid.appendChild(newCell);
 
     }
 
+}
+
+function GrowGrid(){
+
+    // increase number of cells by 8, to a maximum of 'gridMax'
+
+    if (gridSize == gridMax) {
+        ResetGrid();
+        return;
+    }
+
+    const grid = document.getElementById("gridContainer");
+
+    gridSize = gridSize + 8;
+    cellSize = windowSize / gridSize;
+    numberOfCells = gridSize * gridSize;
+
+    // adjust CSS grid to match new size
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    ResetGrid();
+
+}
+
+function ShrinkGrid(){
+
+    // increase number of cells by 8, to a maximum of 'gridMax'
+
+    if (gridSize == gridMin) {
+        ResetGrid();
+        return;
+    }
+
+    const grid = document.getElementById("gridContainer");
+
+    gridSize = gridSize - 8;
+    cellSize = windowSize / gridSize;
+    numberOfCells = gridSize * gridSize;
+
+    // adjust CSS grid to match new size
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    ResetGrid();
+}
+
+function ResetGrid() {
+    
+    // Remove all grid cells from the container, then rebuild. 
+    const grid = document.querySelector('#gridContainer');
+
+    while (grid.firstChild) {
+        grid.removeChild(grid.lastChild);
+    }
+
+    PopulateGrid();
 }
 
