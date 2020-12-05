38127e3f15ac93811b045199f7cbf3fcc18ba9d5
rampagingsquirrel <nicholas.doucette@gmail.com>
2020-12-05 01:40:44 -0500

    Added logic to automatically populate the grid

---
+++ etch-a-sketch.js
@@ -0,0 +1,28 @@
+
+const windowSize = 480; // Gameplay window size (both height & width)
+const gridMin = 8;
+const gridMax = 96;
+
+// Default values for grid size & cells (both height & width)
+// cell size is determined by windowsize / gridsize (480/16 = 30)
+let gridSize = 16;
+let cellsize = 30;
+let numberOfCells = 256;
+
+PopulateGrid();
+
+function PopulateGrid() {
+
+    const grid = document.querySelector('#container');
+    
+        for (i=1; i<=numberOfCells; i++) {
+
+        let newCell = document.createElement('div');
+        newCell.className = "gridCell";
+
+        grid.appendChild(newCell);
+
+    }
+
+}
+
